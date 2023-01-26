c72b93f6a4f1ac24f3364b81e8cde5353844dfef
JudeDavis1 <51873868+JudeDavis1@users.noreply.github.com>
2023-01-26 20:38:46 +0000

    Update bigram_transformer.py

--- bigram_transformer.py
+++ bigram_transformer.py
@@ -13,8 +13,8 @@ learning_rate = 3e-4
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 1
 n_embd = 384
-n_layers = 8
-n_head = 16
+n_layers = 6
+n_head = 6
 dropout = 0.2
 transformer_model_name = 'Bigram-Transformer.pt'
 
