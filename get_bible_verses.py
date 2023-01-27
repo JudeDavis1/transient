8130946c2639473ca55460e9d5399d3702f15e11
Jude Davis <judedavis92@gmail.com>
2023-01-27 17:03:11 +0000

    refactor: bible verse networking improvement

--- get_bible_verses.py
+++ get_bible_verses.py
@@ -13,9 +13,14 @@ def getVerse(b: int, c: int, v: int) -> Tuple[dict, int]:
     req = requests.get(f'https://wol-api.onrender.com/api/v1/bibleVerses/getVerse/{b}/{c}/{v}')
     return (req.json(), req.status_code)
 
+def save_verses(verses: list):
+    with open('./bible_verses.txt', 'a') as f:
+        f.writelines(verses)
+
 
 verses = []
-for book in (t := tqdm(range(1, max_books + 1))):
+pbar = tqdm(range(1, max_books + 1))
+for book in pbar:
     n_chapters_req = requests.get(f'https://wol-api.onrender.com/api/v1/bibleVerses/getNumberOfChapters/{book}')
     n_chapters = int(n_chapters_req.json()['data'])
 
@@ -25,12 +30,13 @@ for book in (t := tqdm(range(1, max_books + 1))):
         
         for verse in range(1, n_verses + 1):
             try:
-                t.set_description(f'{book}/{chapter}/{verse}')
+                pbar.set_description(f'{book}/{chapter}/{verse}')
                 json, status = getVerse(book, chapter, verse)
 
                 verses.append(json['data'])
 
                 # cache verses into a file to save
-                if len(verses) >= 
+                if len(verses) >= cache_interval:
+                    save_verses(verses)
             except:
                 continue
