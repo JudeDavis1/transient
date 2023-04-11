6dc5172d5b75b8d53cab27c84406ccc588da0a10
Jude Davis <judedavis92@gmail.com>
2023-04-11 18:47:14 +0530

    better norm

--- src/model/train.py
+++ src/model/train.py
@@ -83,7 +83,7 @@ def main():
             loss: torch.Tensor = loss / args.gradient_acc
 
         loss.mean().backward()
-        nn.utils.clip_grad.clip_grad_norm(runner.model.parameters(), 1e-4)
+        nn.utils.clip_grad.clip_grad_norm_(runner.model.parameters(), max_norm=1.0)
         total_loss += loss.mean().item()
         scheduler.step()
 
