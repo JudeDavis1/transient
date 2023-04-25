84ef3d021ce647242f82682536a451bf2dd35567
Jude Davis <judedavis92@gmail.com>
2023-04-25 17:28:13 +0100

    score accuracy in eval

--- src/model/evaluate.py
+++ src/model/evaluate.py
@@ -18,6 +18,9 @@ def main():
         runner.model.eval()
         runner.load("model_cache", map_location=cpu_device)
 
+        accuracy = runner.score_accuracy(dataset, n_samples=100)
+        logger.info(f"Accuracy: {accuracy}")
+
         while True:
             context_str = input("> ")
             context = torch.tensor([dataset.encode(dataset.tokenize(context_str))])
