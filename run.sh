44c04e887dca6da216340c2c65954145abadadc7
Jude Davis <judedavis92@gmail.com>
2023-07-01 23:14:27 +0100

    update train script

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
-pip3 install -r requirements.txt
-python3 -m src.model.train -ga 1 -mp 1 -e 3 --device cuda -b 64
+pip3 install pipenv
+pipenv --python $(which python3)
+pipenv install
+
+pipenv run train -ga 1 -mp 1 -e 3 --device cuda -b 40
