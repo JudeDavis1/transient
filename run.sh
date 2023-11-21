2566219035db48b02fec0d24717e4b0fae3c889e
Jude Davis <judedavis92@gmail.com>
2023-11-21 10:58:38 +0000

    ifx:

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 
 device=$1
 
-pipenv run train -ga 1 -mp -e 1 --device $device -b 1 --lr 0.0005 -d 0.0 --from-pretrained model_cache
+pipenv run train -ga 1 -mp -e 1 --device $device -b 1 --lr 0.0003 -d 0.0 --from-pretrained model_scache
